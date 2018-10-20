6369b7832de0852bbefc3b8fe5b549b8a2c179f9
Jayaram <rush2jrp@gmail.com>
2018-10-20 07:51:44 +0530

    Added theme change options

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ var Live = require("./views/Live");
 var Pro = require("./views/Pro");
 var Pub = require("./views/Pub");
 var Footer = require("./views/Footer");
+var Settings = require("./views/Settings");
 var dummy = require("./utils/Dummy");
 
 dummy.timestamp = moment().format("X");
@@ -13,17 +14,38 @@ dummy.timestamp = moment().format("X");
 m.route(document.getElementById("main"), "/live", {
   "/live": {
     render: function() {
-      return m(Layout, [m(Live, {key: dummy.timestamp}), m(Footer, {key: dummy.timestamp})]);
+      return m(Layout, [
+        m(Live, {key: dummy.timestamp}),
+        m(Footer, {key: dummy.timestamp})
+      ]);
     }
   },
   "/pro": {
     render: function() {
-      return m(Layout, [m(Pro, {key: dummy.timestamp}), m(Footer, {key: dummy.timestamp})]);
+      return m(Layout, [
+        m(Pro, {key: dummy.timestamp}),
+        m(Footer, {key: dummy.timestamp})
+      ]);
     }
   },
   "/pub": {
     render: function() {
-      return m(Layout, [m(Pub, {key: dummy.timestamp}), m(Footer, {key: dummy.timestamp})]);
+      return m(Layout, [
+        m(Pub, {key: dummy.timestamp}),
+        m(Footer, {key: dummy.timestamp})
+      ]);
     }
   }
 });
+
+
+m.mount(document.getElementById("settings"), Settings);
+m.mount(document.getElementById("cog"), {
+  view: function() {
+    return m("span.#cog.c-hand", {onclick: function() {Settings.active=true;}}, [
+      m("img", {
+        src: "https://res.cloudinary.com/metahero/image/upload/v1539997876/cog.svg"
+      })
+    ]);
+  }
+})
---
+++ src/views/Settings.js
@@ -0,0 +1,64 @@
+var m = require("mithril");
+
+var Settings = {
+  active: false,
+  theme: "light",
+  setTheme: function (value) {
+    Settings.theme = value;
+    if(value === "dark") {
+      document.body.classList.add("theme-dark");
+      window.localStorage.setItem("theme", "dark");
+    } else {
+      document.body.classList.remove("theme-dark");
+      window.localStorage.setItem("theme", "light");
+    }
+  },
+  oninit: function() {
+    var theme = localStorage.getItem("theme");
+    if (theme) {
+      Settings.theme = theme;
+      Settings.setTheme(theme);
+    }
+  },
+  view: function() {
+    return m("div#modal-id.modal.modal-sm", {class: Settings.active?"active":""}, [
+      m("a.modal-overlay", {
+          "href": "#close",
+          "aria-label": "Close",
+          "className": "modal-overlay",
+      }),
+      m("div.modal-container", [
+        m("div.modal-header", [
+          m("a.btn.btn-clear.float-right", {
+              "href": "#close",
+              "aria-label": "Close",
+              "className": "btn btn-clear float-right",
+              onclick: function() {Settings.active = false;}
+          }),
+          m("div.modal-title.h5", `Settings`)
+        ]),
+        m("div.modal-body", [
+          m("div.content", [
+            m("label", [
+              m("input[type='radio'][name='theme']", {
+                value: "light",
+                checked: Settings.theme === "light"?true:false,
+                onclick: m.withAttr("value", Settings.setTheme),
+              }),
+            ], " light"), m("br"),
+            m("label", [
+              m("input[type='radio'][name='theme']", {
+                value: "dark",
+                checked: Settings.theme === "dark"?true:false,
+                onclick: m.withAttr("value", Settings.setTheme),
+              }),
+            ], " dark"),
+          ])
+        ]),
+        m("div.modal-footer")
+      ])
+    ]);
+  }
+};
+
+module.exports = Settings;
